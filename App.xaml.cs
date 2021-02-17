af02c6e1335152f6cd8164d247ca17735cb4dd20
Cristian Creanga <creanga.ccc@gmail.com>
2021-02-18 01:13:47 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Cristian_Creanga_Aplicatie_Vaccinare
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
